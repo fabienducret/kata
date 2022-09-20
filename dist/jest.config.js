bbdd1f1ee06ac2bd864c6854f92edfccdca4a99f
Fabien Ducret <>
2022-09-20 12:42:30 +0200

    feat: conf jest

--- dist/jest.config.js
+++ dist/jest.config.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
+    preset: 'ts-jest/presets/js-with-ts-esm',
+    globals: { 'ts-jest': { useESM: true, isolatedModules: true } },
     roots: ['<rootDir>/src'],
+    extensionsToTreatAsEsm: ['.ts'],
 };
--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,6 @@
 export default {
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  globals: { 'ts-jest': { useESM: true, isolatedModules: true } },
   roots: ['<rootDir>/src'],
+  extensionsToTreatAsEsm: ['.ts'],
 };
